bfa11e507257eb0da8ab162675930c89f83edf11
Vid141 <75320995+Vid141@users.noreply.github.com>
2023-06-13 15:59:29 +0530

    Add files via upload

---
+++ Trex_Stage_1-main/sketch.js
@@ -0,0 +1,20 @@
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+
+function preload(){
+  
+}
+
+function setup() {
+  createCanvas(600, 200);
+  
+  //create a trex sprite
+ 
+}
+
+function draw() {
+  background(220);
+  
+  
+  drawSprites();
+}
